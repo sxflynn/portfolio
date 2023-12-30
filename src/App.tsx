1902134982ed331d50f9b8a0a2277c8baf304126
Stephen Flynn <sxflynn@users.noreply.github.com>
2023-12-30 04:06:57 +0000

    initial commit on the portfolio from mantine template

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider } from '@mantine/core';
 import { theme } from "./theme";
+import { Header } from './components/Header/Header';
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return <MantineProvider theme={theme}>
+      <Header/>
+
+      
+      {/* ROUTER VIEW? */}
+      {/* use a customized hero header for each project description */}
+
+      {/* FOOTER */}
+
+
+      </MantineProvider>
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { Container, Group, Burger, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import classes from './Header.module.css';
+
+const links = [
+  { link: '/projects', label: 'Projects' },
+  { link: '/about', label: 'about' },
+  { link: '/linkedin', label: 'LinkedIn' },
+  { link: '/github', label: 'GitHub' },
+];
+
+export function Header() {
+  const [opened, { toggle }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
+
+  const items = links.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={classes.link}
+      data-active={active === link.link || undefined}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(link.link);
+      }}
+    >
+      {link.label}
+    </a>
+  ));
+
+  return (
+    <header className={classes.header}>
+      <Container size="md" className={classes.inner}>
+      <Text size="xl">Stephen Flynn</Text>
+        <Group gap={5} visibleFrom="xs">
+          {items}
+        </Group>
+
+        <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
+      </Container>
+    </header>
+  );
+}
