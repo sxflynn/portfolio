8d25b4adb8d430b026ec094bf10faf146070e0d9
Stephen Flynn <stephenflynn@me.com>
2024-01-08 15:36:36 -0500

    added new video element to project page. updated project text

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,19 +1,20 @@
 import { Container, Title, Button, Group, Text, List, ThemeIcon, rem, Code, Image } from '@mantine/core';
-import { Link } from 'react-router-dom';
-import { IconCheck } from '@tabler/icons-react';
+import { IconCheck, IconBrandPython, IconApi, IconBrandReact, IconExternalLink } from '@tabler/icons-react';
 import classes from './Projects.module.css'
+import ExternalLink from '../../components/ExternalLink';
+import ballotBearDemo from '../../assets/ballotbear-demo.mp4';
 
 const Projects = () => {
 
     return (
-        <Container size="md">
+        <Container size="lg">
             <div className={classes.inner}>
                 <div className={classes.content}>
                     <Title className={classes.title}>
                         Upper Arlington School Board Election Simulator
                     </Title>
                     <Text c="dimmed" mt="md">
-                        I built a Python web application to help an Upper Arlington voter predict who will win the 2023 School Board election.
+                        React/Python web application to help an Upper Arlington voter predict who will win the November 2023 School Board election.
                     </Text>
 
                     <List
@@ -21,37 +22,70 @@ const Projects = () => {
                         spacing="sm"
                         size="sm"
                         icon={
-                            <ThemeIcon size={20} radius="xl">
-                                <IconCheck style={{ width: rem(12), height: rem(12) }} stroke={1.5} />
+                            <ThemeIcon size={"lg"} radius="lg" variant="light">
+                                <IconCheck style={{ width: rem(24), height: rem(24) }} stroke={1.5} />
                             </ThemeIcon>
                         }
                     >
-                        <List.Item>
-                            <b>Backend written in Python</b> – object oriented
+                        <List.Item
+                            icon={
+                                <ThemeIcon size={"lg"} radius="xl" variant="light">
+                                    <IconBrandPython style={{ width: rem(24), height: rem(24) }} stroke={1.5} />
+                                </ThemeIcon>
+                            }>
+                            <b>Prediction engine written in Python</b> – Business logic abstracted from implementation, allowing election engine to run both as a CLI or a web app
                         </List.Item>
-                        <List.Item>
+                        <List.Item
+                            icon={
+                                <ThemeIcon size={"lg"} radius="xl" variant="light">
+                                    <IconApi style={{ width: rem(24), height: rem(24) }} stroke={1.5} />
+                                </ThemeIcon>
+                            }>
                             <b>Served with FastAPI</b> – Election configuration and candidate information is validated with Pydantic models
                         </List.Item>
-                        <List.Item>
-                            <b>React frontend</b> – User preferences stored in <Code>LocalStorage</Code>. Soon: React Router, UI Library
+                        <List.Item
+                            icon={
+                                <ThemeIcon size={"lg"} radius="xl" variant="light">
+                                    <IconBrandReact style={{ width: rem(24), height: rem(24) }} stroke={1.5} />
+                                </ThemeIcon>
+                            }>
+                            <b>React frontend</b> – User preferences stored in <Code>LocalStorage</Code>. Built with Vite.
                         </List.Item>
                     </List>
 
                     <Group mt={30}>
-                        <Link to="http://www.ballotbear.app">
-                            <Button radius="xl" size="md" className={classes.control}>
+                        <ExternalLink to="http://www.ballotbear.app">
+                            <Button 
+                            radius="xl" 
+                            size="md" 
+                            className={classes.control} 
+                            rightSection={<IconExternalLink/>}
+                            >
                                 Visit BallotBear.app
                             </Button>
-                        </Link>
-                        <Link to="http://www.github.com/sxflynn/electionsim">
-                            <Button variant="default" radius="xl" size="md" className={classes.control}>
-                                GitHub Source code
+                        </ExternalLink>
+                        <ExternalLink to="http://www.github.com/sxflynn/electionsim">
+                            <Button 
+                            variant="default" 
+                            radius="xl" 
+                            size="md" 
+                            className={classes.control}
+                            rightSection={<IconExternalLink/>}
+                            >
+                                GitHub repo
                             </Button>
-                        </Link>
+                        </ExternalLink>
                     </Group>
                 </div>
-                {/* Image goes here */}
-                Image
+
+                <div className={classes.video}>
+                    <video autoPlay muted loop playsInline>
+                        <source src={ballotBearDemo} type="video/mp4"/>
+                    </video>
+
+
+                </div>
+
             </div>
 
         </Container>
