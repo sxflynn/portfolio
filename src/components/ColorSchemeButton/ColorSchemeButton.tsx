9fd6e677fd483d3b9278ae0e0ea45729719713e0
Stephen Flynn <stephenflynn@me.com>
2024-01-13 09:51:35 -0500

    updated color scheme button with x clear, inspired by Josh Goldberg's portfolio

--- src/components/ColorSchemeButton/ColorSchemeButton.tsx
+++ src/components/ColorSchemeButton/ColorSchemeButton.tsx
@@ -1,20 +1,43 @@
 
-import { Text, Button, useMantineColorScheme } from '@mantine/core';
-import { IconMoon, IconSun } from '@tabler/icons-react';
+import { Button, Group, useMantineColorScheme } from '@mantine/core';
+import { IconMoon, IconSun, IconX } from '@tabler/icons-react';
 
 export function ColorSchemeButton() {
-  const {colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const {colorScheme, toggleColorScheme, clearColorScheme } = useMantineColorScheme();
+  const isAuto = colorScheme === 'auto';
   const isDark = colorScheme === 'dark';
+
+// ClearButton UI inspired by how Josh Goldberg handles auto themes on his portfolio at https://www.joshuakgoldberg.com
+  const ClearButton = () => (
+    <Button
+      variant="transparent"
+      onClick={clearColorScheme}
+      title="Reset to auto color scheme"
+      pos="absolute"
+      ml={50}
+      mb={40}
+      color='primary'
+      >
+        <IconX
+        size={14}
+        />
+    </Button>
+  );
+
   return (
    <>
   
-      <Button 
-      variant="default"
-      onClick={() => toggleColorScheme()}
-      title={isDark ? "Switch to light mode" : "Switch to dark mode"}
-      >
-        {isDark ? <IconSun /> : <IconMoon />}
-      </Button>
+      <Group>
+        <Button 
+        variant="default"
+        onClick={() => toggleColorScheme()}
+        title={isDark ? "Switch to light mode" : "Switch to dark mode"}
+        >
+          {isDark ? <IconSun /> : <IconMoon />}
+        </Button>
+        {!isAuto && <ClearButton />}
+      </Group>
+
    </>
   )
 }
