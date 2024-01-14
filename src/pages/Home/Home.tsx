814016474b4c26eb6542477be0e09ea33348a6ad
Stephen Flynn <stephenflynn@me.com>
2024-01-14 13:04:03 -0500

    replaced list with cards on homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,33 +9,46 @@ import {
   ThemeIcon,
   rem,
   Anchor,
+  Grid,
+  Card,
+  Badge,
+  SimpleGrid,
 } from "@mantine/core";
 import classes from "./Home.module.css";
-import { IconBuildingSkyscraper, IconCheck, IconDownload } from "@tabler/icons-react";
+import {
+  IconBackpack,
+  IconBrandPython,
+  IconBuildingSkyscraper,
+  IconCheck,
+  IconCloudComputing,
+  IconDownload,
+  IconPresentationOff,
+  IconSql,
+} from "@tabler/icons-react";
 
-import StephenXFlynnResume from "../../assets/StephenXFlynnResume.pdf";
 import { Link } from "react-router-dom";
 
-const FlipText = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const skills = ["Java", "full stack", "React", "Python"];
+// unused animation
+// const FlipText = () => {
+//   const [currentIndex, setCurrentIndex] = useState(0);
+//   const skills = ["Java", "full stack", "React", "Python"];
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % skills.length);
-    }, 2000);
+//   useEffect(() => {
+//     const intervalId = setInterval(() => {
+//       setCurrentIndex((prevIndex) => (prevIndex + 1) % skills.length);
+//     }, 2000);
 
-    return () => clearInterval(intervalId);
-  }, []);
+//     return () => clearInterval(intervalId);
+//   }, []);
 
-  return <span className={classes.changeText}>{skills[currentIndex]}</span>;
-};
+//   return <span className={classes.changeText}>{skills[currentIndex]}</span>;
+// };
 
 const Home = () => {
   return (
     <Container size="md" className={classes.inner}>
       <h1 className={classes.title}>
-        Hi! 👋 I'm Stephen X. Flynn, a {" "}
+        Hi! 👋 I'm Stephen X. Flynn, a{" "}
         <Text
           component="span"
           variant="gradient"
@@ -47,14 +60,15 @@ const Home = () => {
         based in Columbus, Ohio. <IconBuildingSkyscraper size={30} />
       </h1>
       <Text size="lg">
-        I'm passionate about creating web applications
-        with Java, Spring Boot, PostgreSQL, and JavaScript frameworks like Vue
-        and React. I have built a strong foundation of teamwork, troubleshooting
-        and innovation from my previous successful careers in classroom teaching
-        and technology librarianship.
+        I'm passionate about creating web applications with Java, Spring Boot,
+        PostgreSQL, and JavaScript frameworks like Vue and React. I have built a
+        strong foundation of teamwork, troubleshooting and innovation from my
+        previous successful careers in classroom teaching and technology
+        librarianship.
       </Text>
+
       <Group mt="lg" mb="lg">
-        <Anchor href={StephenXFlynnResume}>
+        <Anchor href='https://pub-0d39c7af531f4456b2a0bb4b5d58306e.r2.dev/StephenXFlynnResume.pdf'>
           <Button rightSection={<IconDownload size={16} />}>View Resume</Button>
         </Anchor>
         <Link to="/projects">
@@ -68,8 +82,77 @@ const Home = () => {
       <Text size="lg" mt="md" mb="md" fw="bold">
         Software Highlights
       </Text>
+      <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }}>
+        <div>
+          <Card shadow="sm" padding="md" radius="lg" withBorder>
+            <Grid>
+              <Grid.Col span={{ base: 1, lg: 2 }}>
+                <ThemeIcon color="orange" variant="light">
+                  {" "}
+                  <IconBrandPython
+                    style={{ width: rem(20), height: rem(20) }}
+                    stroke={2}
+                  />
+                </ThemeIcon>
+              </Grid.Col>
+              <Grid.Col span={{ base: 10, lg: 10 }}>
+                <Text mt="10px">
+                  Within two weeks, created an Upper Arlington school board
+                  election predictor in Python, with a React frontend, deployed
+                  to Vercel. Correctly predicted election.
+                </Text>
+              </Grid.Col>
+            </Grid>
+          </Card>
+        </div>
 
-      <List
+        <div>
+          <Card shadow="sm" padding="md" radius="lg" withBorder>
+            <Grid>
+              <Grid.Col span={{ base: 1, lg: 2 }}>
+                <ThemeIcon color="red" variant="light">
+                  {" "}
+                  <IconSql
+                    style={{ width: rem(20), height: rem(20) }}
+                    stroke={2}
+                  />
+                </ThemeIcon>
+              </Grid.Col>
+              <Grid.Col span={{ base: 10, lg: 10 }}>
+                <Text mt="10px">
+                  Within two weeks, led a team to create a web application to
+                  help teachers cover their shifts, using Java, PostgreSQL,
+                  Vue.js, in an Agile work environment
+                </Text>
+              </Grid.Col>
+            </Grid>
+          </Card>
+        </div>
+
+        <div>
+          <Card shadow="sm" padding="md" radius="lg" withBorder>
+            <Grid>
+              <Grid.Col span={{ base: 1, lg: 2 }}>
+                <ThemeIcon color="blue" variant="light">
+                  {" "}
+                  <IconBackpack
+                    style={{ width: rem(20), height: rem(20) }}
+                    stroke={2}
+                  />
+                </ThemeIcon>
+              </Grid.Col>
+              <Grid.Col span={{ base: 10, lg: 10 }}>
+                <Text mt="10px">
+                  Graduated from intensive 3 month software development
+                  bootcamp, learning object oriented programming, full stack
+                  development through 800 hours of instruction
+                </Text>
+              </Grid.Col>
+            </Grid>
+          </Card>
+        </div>
+      </SimpleGrid>
+      {/* <List
         spacing="sm"
         size="md"
         icon={
@@ -79,18 +162,19 @@ const Home = () => {
         }
       >
         <ListItem>
-          Within two weeks, created a local election predictor in Python, with a React frontend,
-          deployed to Vercel
+          Within two weeks, created a local election predictor in Python, with a
+          React frontend, deployed to Vercel
         </ListItem>
         <ListItem>
-          Within two weeks, led a team to create a web application to help teachers cover their
-          shifts, using Java, PostgreSQL, Vue.js, in an Agile work environment
+          Within two weeks, led a team to create a web application to help
+          teachers cover their shifts, using Java, PostgreSQL, Vue.js, in an
+          Agile work environment
         </ListItem>
         <ListItem>
           Graduated from intensive 3 month software development bootcamp,
           learning object oriented development, test driven development
         </ListItem>
-      </List>
+      </List> */}
 
       <Text size="lg" mt="md" mb="md" fw="bold">
         Career Highlights
@@ -105,12 +189,12 @@ const Home = () => {
           </ThemeIcon>
         }
       >
-        <ListItem>Tech Elevator full stack bootcamp graduate</ListItem>
+        <ListItem>Tech Elevator full stack coding boot camp graduate</ListItem>
         <ListItem>
-          7 years middle school teacher, promoted to Technology Chair and Grade
+          7 years middle history school teacher, promoted to Technology Chair and Grade
           Level Chair
         </ListItem>
-        <ListItem>5 year career as Emerging Technologies Librarian</ListItem>
+        <ListItem>5 years as Emerging Technologies Librarian</ListItem>
         <ListItem>
           Master's Degrees in Computer Education Technology, and Library
           Information Science
