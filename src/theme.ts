0865fe85668852ad35ef53d9f5f10549959f0a9b
Stephen Flynn <stephenflynn@me.com>
2024-01-15 15:35:38 -0500

    added custom breakpoint for xs to fix header ui bug

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,11 @@
 import { createTheme } from "@mantine/core";
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  breakpoints: {
+    xs: '38em',
+    sm: '48em',
+    md: '62em',
+    lg: '75em',
+    xl: '88em',
+  },
 });
