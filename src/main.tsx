bf25dd182cde30812b34b54d65767051696740d9
Stephen Flynn <stephenflynn@me.com>
2024-01-15 16:44:48 -0500

    added 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import App from "./App.tsx";
 import About from "./pages/About/About.tsx";
 import Home from "./pages/Home/Home.tsx";
 import Projects from "./pages/Projects/Projects.tsx";
-
+import PageNotFound from "./pages/PageNotFound/PageNotFound.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-       <Route index element={<Home />} /> {/* Add Home component as the index route */}
+      <Route index element={<Home />} />{" "}
+      {/* Add Home component as the index route */}
       <Route path="projects" element={<Projects />} />
       <Route path="about" element={<About />} />
       {/* Add more nested routes if needed */}
+      <Route path="*" element={<PageNotFound />} />
     </Route>
   )
 );
