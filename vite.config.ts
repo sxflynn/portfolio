14f9e2c5f551f3e1640478794bc7e9f33ac5b9c9
Stephen Flynn <stephenflynn@me.com>
2025-02-05 23:54:30 -0500

    Fix for tabler icon chunks bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+    },
+  },
   server: {
-   watch: {
-    usePolling: true,
-   },
-   host: true, // Here
-   strictPort: true,
-   port: 5173, 
- }})
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Here
+    strictPort: true,
+    port: 5173,
+  },
+});
