9fb3462abfe46f7681938706851ebd0a4aa64532
Stephen Flynn <stephenflynn@me.com>
2025-02-07 18:55:15 -0500

    Prettier changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import "@mantine/core/styles.css";
-import '@mantine/code-highlight/styles.css';
-import { MantineProvider, localStorageColorSchemeManager } from '@mantine/core';
+import "@mantine/code-highlight/styles.css";
+import { MantineProvider, localStorageColorSchemeManager } from "@mantine/core";
 import { theme } from "./theme";
-import { Header } from './components/Header/Header';
-import { Outlet } from 'react-router-dom';
+import { Header } from "./components/Header/Header";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
-  return <MantineProvider
-    theme={theme}
-    defaultColorScheme="auto"
-    colorSchemeManager={localStorageColorSchemeManager({ key: 'portfolio-color-scheme' })}
-  >
-    <Header />
-    <Outlet />
-    {/* ADD FOOTER */}
-  </MantineProvider>
+  return (
+    <MantineProvider
+      theme={theme}
+      defaultColorScheme="auto"
+      colorSchemeManager={localStorageColorSchemeManager({
+        key: "portfolio-color-scheme",
+      })}
+    >
+      <Header />
+      <Outlet />
+      {/* ADD FOOTER */}
+    </MantineProvider>
+  );
 }
