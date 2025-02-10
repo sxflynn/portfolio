b83e0b838726f2a00798299614574b06efbcadb3
Stephen Flynn <stephenflynn@me.com>
2025-02-09 19:41:36 -0500

    [CI Skip] Redo stylelint config due to .stylelintrc.json being deprecated

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,7 @@
+/** @type {import('stylelint').Config} */
+export default 
 {
-  "extends": ["stylelint-config-standard-scss"],
+  extends: ["stylelint-config-standard-scss"],
   "rules": {
     "custom-property-pattern": null,
     "selector-class-pattern": null,
